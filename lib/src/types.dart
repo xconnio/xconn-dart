7f5ed20e15d1dbfd1730c03f3e2aae94164f854e
Muzzammil Shahid <muzzammil@xconn.io>
2024-05-10 19:40:17 +0500

    Add ClientSideLocalBaseSession

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -1,9 +1,12 @@
 import "dart:async";
+import "dart:collection";
 import "dart:io";
 
 import "package:wampproto/messages.dart";
 import "package:wampproto/serializers.dart";
 import "package:wampproto/session.dart";
+
+import "package:xconn/src/router.dart";
 
 abstract class IBaseSession {
   int id() {
@@ -194,4 +197,66 @@ class UnsubscribeRequest {
 
   final Completer<void> future;
   final int subscriptionId;
+}
+
+class ClientSideLocalBaseSession implements IBaseSession {
+  ClientSideLocalBaseSession(this._id, this._realm, this._authid, this._authrole, this._serializer, this._router) {
+    _incomingMessages = Queue();
+    _completer = Completer();
+  }
+
+  final int _id;
+  final String _realm;
+  final String _authid;
+  final String _authrole;
+  final Serializer _serializer;
+  final Router _router;
+
+  late Queue _incomingMessages;
+  late Completer _completer;
+
+  @override
+  int id() => _id;
+
+  @override
+  String realm() => _realm;
+
+  @override
+  String authid() => _authid;
+
+  @override
+  String authrole() => _authrole;
+
+  @override
+  serializer() => _serializer;
+
+  @override
+  Future send(Object data) async {
+    await sendMessage(_serializer.deserialize(data));
+  }
+
+  @override
+  Future<Object> receive() async {
+    await _completer.future;
+    _completer = Completer();
+    return _incomingMessages.removeFirst();
+  }
+
+  @override
+  Future<void> sendMessage(Message msg) async {
+    return _router.receiveMessage(this, msg);
+  }
+
+  @override
+  Future<Message> receiveMessage() async {
+    return _serializer.deserialize(await receive());
+  }
+
+  @override
+  Future close() async {}
+
+  Future feed(Object data) async {
+    _incomingMessages.add(data);
+    _completer.complete();
+  }
 }
