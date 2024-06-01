b16560367f455f51beab0c7605607abc90340534
Muzzammil Shahid <muzzammil@xconn.io>
2024-06-01 18:40:04 +0500

    Add optional authenticator parameter to start method and sync auth changes

--- lib/src/acceptor.dart
+++ lib/src/acceptor.dart
@@ -3,6 +3,7 @@ import "dart:io";
 
 import "package:wampproto/acceptor.dart";
 import "package:wampproto/auth.dart";
+import "package:wampproto/messages.dart";
 import "package:wampproto/serializers.dart";
 
 import "package:xconn/src/helpers.dart";
@@ -28,14 +29,21 @@ class WAMPSessionAcceptor {
         MapEntry<Object, bool> received = acceptor.receive(message);
         ws.add(received.key);
         if (received.value) {
-          wsStreamSubscription
-            ..onData(null)
-            ..onDone(null);
+          if (acceptor.isAborted()) {
+            ws.close();
+            var abortMessage = _serializer.deserialize(received.key) as Abort;
+            completer.completeError(Exception(abortMessage.reason));
+          } else {
+            wsStreamSubscription
+              ..onData(null)
+              ..onDone(null);
 
-          var base = BaseSession(ws, wsStreamSubscription, acceptor.getSessionDetails(), _serializer);
-          completer.complete(base);
+            var base = BaseSession(ws, wsStreamSubscription, acceptor.getSessionDetails(), _serializer);
+            completer.complete(base);
+          }
         }
       } on Exception catch (error) {
+        ws.close();
         completer.completeError(error);
       }
     });
